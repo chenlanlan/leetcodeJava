ddecba28015030e02247b2de015d8629400c11a2
chenlanlan <silan0318@163.com>
2015-07-06 16:07:46 -0700

    new

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int sum = 0;
+        while (left < right){
+            int water = (right - left) * Math.min(height[left], height[right]);
+            if (water > sum){
+                sum = water;
+            }
+            if (height[left] < height[right]){
+                left ++;
+            }else{
+                right --;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        ContainerWithMostWater test = new ContainerWithMostWater();
+        int[] height = {3, 1, 4, 5};
+        System.out.println(test.maxArea(height));
+    }
+}
+
