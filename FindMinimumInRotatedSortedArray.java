949e97e37e4c2f65c3003bb65283bfd7c341d77e
chenlanlan <silan0318@163.com>
2015-07-06 16:08:20 -0700

    new

---
+++ FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,22 @@
+
+public class FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right){
+            if (nums[left] < nums[right]) return nums[left];
+            int mid = (left + right) / 2;
+            if (nums[left] <= nums[mid]){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+    public static void main(String[] args){
+        FindMinimumInRotatedSortedArray test = new FindMinimumInRotatedSortedArray();
+        int[] nums = {5, 0, 1, 2};
+        System.out.println(test.findMin(nums));
+    }
+}
+
