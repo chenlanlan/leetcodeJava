2a27bc4cba6995bd9630b4d0bb11dbb94710a34f
chenlanlan <silan0318@163.com>
2015-07-07 10:15:34 -0700

    new

---
+++ MajorityElement.java
@@ -0,0 +1,22 @@
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int count = 0, candidate = 0;
+        for (int e : nums){
+            if (count == 0){
+                candidate = e;
+                count += 1;
+            }else if (e == candidate){
+                count += 1;
+            }else{
+                count -= 1;
+            }
+        }
+        return candidate;
+    }
+    public static void main(String[] args){
+        MajorityElement test = new MajorityElement();
+        int[] nums = {1, 2, 3, 1, 1,2};
+        System.out.println(test.majorityElement(nums));
+    }
+}
