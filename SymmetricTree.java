f52e5ab59ce8e6050abecc200af1597f3713f8a5
chenlanlan <silan0318@163.com>
2015-07-09 10:08:56 -0700

    newe

---
+++ SymmetricTree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return determin(root.left, root.right);
+    }
+    public boolean determin(TreeNode p, TreeNode q){
+        if (p == null && q == null) return true;
+        if (p != null && q != null && p.val == q.val){
+            return determin(p.left, q.right) && determin(p.right, q.left);
+        }
+        return false;
+    }
+}
