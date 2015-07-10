9b0917dd21f7bb96cb3019d21e48a5733906820a
chenlanlan <silan0318@163.com>
2015-07-10 10:05:32 -0700

    new

---
+++ BinaryTreePostorderTraversal.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        postorder(root, res);
+        return res;
+    }
+    private void postorder(TreeNode root, List<Integer> res){
+        if (root == null) return;
+        postorder(root.left, res);
+        postorder(root.right, res);
+        res.add(root.val);        
+    }
+}
+
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) return res;
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        HashSet<TreeNode> hs = new HashSet<TreeNode>();
+        st.push(root);
+        while (!st.isEmpty()){
+            TreeNode temp = st.pop();
+            if (hs.contains(temp)){
+                res.add(temp.val);
+                continue;
+            }
+            hs.add(temp);
+            st.push(temp);
+            if (temp.right != null){
+                st.push(temp.right);
+            }
+            if (temp.left != null){
+                st.push(temp.left);
+            }
+        }
+        return res;
+        
+    }
+}
