4f4bd973a1a2fac2ec061c802fce106ca5855662
chenlanlan <silan0318@163.com>
2015-07-11 09:59:12 -0700

    new

---
+++ Permutations.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (nums.length == 0 || nums == null) return res;
+        findPermutations(nums, 0, res);
+        return res;
+    }
+    public void findPermutations(int[] nums, int index, List<List<Integer>> res){
+        if (index == nums.length) {
+            List<Integer> temp = new ArrayList<Integer>();
+            for(int j = 0; j < nums.length; j++) {
+                temp.add(nums[j]);
+            }
+            res.add(temp);
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            int swap = nums[i];
+            nums[i] = nums[index];
+            nums[index] =swap;
+            findPermutations(nums, index + 1, res);
+            swap = nums[i];
+            nums[i] = nums[index];
+            nums[index] = swap;
+        }
+        return;
+    }
+    public static void main(String[] args){
+        Permutations test = new Permutations();
+        int[] nums = {1, 2, 3};
+        System.out.println(test.permute(nums));
+    }
+}
