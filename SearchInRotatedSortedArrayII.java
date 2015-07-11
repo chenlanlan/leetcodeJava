15bd5bcbda93f410f65d1bc5a8c95b18f03018bc
chenlanlan <silan0318@163.com>
2015-07-11 10:01:48 -0700

    new

---
+++ SearchInRotatedSortedArrayII.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right){
+            int mid = (left + right) / 2 ;
+            if (nums[mid] == target) return true;
+            if (nums[mid] > nums[right]){
+                if (nums[mid] < target || target < nums[left]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }else if (nums[mid] < nums[right]){
+                if (nums[mid] > target || target > nums[right]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                right --;
+            }
+        }
+        return false;
+    }
+}
