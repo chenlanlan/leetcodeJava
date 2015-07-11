2c0cc477fafafc317823b0aa3697e75ad4894ec5
chenlanlan <silan0318@163.com>
2015-07-11 09:58:20 -0700

    new

---
+++ Substring.java
@@ -0,0 +1,15 @@
+
+public class Substring {
+    public void Substring(String a) {
+        int len = a.length();
+        for (int begin = 0; begin < len; begin++){
+            for (int end = begin + 1; end <= len; end++){
+                System.out.println(a.substring(begin, end));
+            }
+        }
+    }
+    public static void main(String[] args){
+        Substring test = new Substring();
+        test.Substring("abc");
+    }
+}
