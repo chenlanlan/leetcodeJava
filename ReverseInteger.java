5138720473e47fe3a3b54e13fc72f44054bf2e97
chenlanlan <silan0318@163.com>
2015-07-13 14:36:09 -0700

    nwe

---
+++ ReverseInteger.java
@@ -0,0 +1,20 @@
+public class ReverseInteger {
+    public int reverse(int x) {
+        boolean F = false;
+        int res = 0;
+        if (x < 0){
+            F = true;
+            x = Math.abs(x);
+        }
+        while (x > 0){
+            if ((long)(res) * 10 > Integer.MAX_VALUE) return 0;
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        return (F)? (-1)*res : res;
+    }
+    public static void main(String[] args){
+        ReverseInteger test = new ReverseInteger();
+        System.out.println(test.reverse(123));
+    }
+}
