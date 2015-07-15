3ffcd99dbd3e52e08a0d51b674326ddcf1bd4607
chenlanlan <silan0318@163.com>
2015-07-14 17:23:52 -0700

    new

---
+++ Anagrams.java
@@ -0,0 +1,27 @@
+public class Anagrams {
+    public List<String> anagrams(String[] strs) {
+        List<String> res = new ArrayList<String>();
+        HashMap<String, Integer> ana = new HashMap<String, Integer>();
+        String temp = "";
+        int index = 0;
+        for (String s : strs){
+            temp = sortstring(s);
+            if (!ana.containsKey(temp)) {
+                ana.put(temp, index);
+            }else{
+                int i = ana.get(temp);
+                if (i != -1){
+                    result.add(strs[i]);
+                }
+                result.add(s);
+            }
+            index++;
+        }
+        return result;
+    }
+    public String sortstring(String s){
+        char[] c = s.toCharArray();
+        Arrays.sort(c);
+        return new String(c);
+    }
+}
