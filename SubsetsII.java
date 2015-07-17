da5dd6d09e0856f86c80bfe7ce6352b712c7bb12
chenlanlan <silan0318@163.com>
2015-07-17 14:36:57 -0700

    modified

--- SubsetsII.java
+++ SubsetsII.java
@@ -1,18 +1,17 @@
 import java.util.List;
 import java.util.ArrayList;
 public class SubsetsII {
-    public ArrayList<ArrayList<Integer>> subsets(int[] nums) {
-        ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> result = new ArrayList<Integer>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        List<Integer> result = new ArrayList<Integer>();
+        Arrays.sort(nums);
         dfs(nums, 0, result, results);
         return results;
     }
-    public void dfs(int[] nums, int idx, ArrayList<Integer> result, ArrayList<ArrayList<Integer>> results) {
+    public void dfs(int[] nums, int index, List<Integer> result, List<List<Integer>> results) {
         results.add(new ArrayList<Integer>(result));
-        for (int i = idx; i < nums.length; i++){
-            if(i != idx && nums[i] == nums[i - 1]){
-                continue;
-            }
+        for (int i = index; i < nums.length; i++) {
+            if (i != index && nums[i] == nums[i - 1]) continue;
             result.add(nums[i]);
             dfs(nums, i + 1, result, results);
             result.remove(result.size() - 1);
