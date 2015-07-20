c89406bb27a3224cc3e5a2361d5367785635a314
chenlanlan <silan0318@163.com>
2015-07-20 16:01:36 -0700

    new

---
+++ Sqrt(x).java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int mySqrt(int x) {
+        double ans = x;
+        while (abs(ans * ans - x) > 0.0001) {
+            ans = (ans + x / ans) / 2;
+        }
+        return (int)ans;
+    }
+    public int mySqrt(int x) {
+        int left = 1, right = x;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid < x / mid) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return (left == x / left)? left : left - 1;
+    }
+}
